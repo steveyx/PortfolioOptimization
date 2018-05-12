db96fe39638e5411eb7664177a086e537264bb96
Steve Cao <steve@yojee.com>
2018-05-12 13:50:57 +0800

    update data reader source

---
+++ portfolio_op.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+import quandl
+import matplotlib.pyplot as plt
+
+#list of stocks in portfolio
+stocks = ['AAPL','MSFT','AMZN','GOOG']
+ 
+#download daily price data for each of the stocks in the portfolio
+#data = web.DataReader(stocks,data_source='google',start='01/01/2010')['Close']
+data = pd.DataFrame()
+for i, stk in enumerate(stocks):
+    s = quandl.get("WIKI/"+stk, start_date='2015-1-1', end_date='2018-1-1')[['Adj. Close']].rename(columns={'Adj. Close':stk})
+    if i == 0:
+        data = s.copy()
+    else:
+        data[stk] = s[stk]
+
+#convert daily stock prices into daily returns
+returns = data.pct_change()
+ 
+#calculate mean daily return and covariance of daily returns
+mean_daily_returns = returns.mean()
+cov_matrix = returns.cov()
+ 
+#set number of runs of random portfolio weights
+num_portfolios = 25000
+ 
+#set up array to hold results
+#We have increased the size of the array to hold the weight values for each stock
+results = np.zeros((4+len(stocks)-1,num_portfolios))
+ 
+for i in range(num_portfolios):
+    #select random weights for portfolio holdings
+    weights = np.array(np.random.random(4))
+    #rebalance weights to sum to 1
+    weights /= np.sum(weights)
+ 
+    #calculate portfolio return and volatility
+    portfolio_return = np.sum(mean_daily_returns * weights) * 252
+    portfolio_std_dev = np.sqrt(np.dot(weights.T,np.dot(cov_matrix, weights))) * np.sqrt(252)
+ 
+    #store results in results array
+    results[0,i] = portfolio_return
+    results[1,i] = portfolio_std_dev
+    #store Sharpe Ratio (return / volatility) - risk free rate element excluded for simplicity
+    results[2,i] = results[0,i] / results[1,i]
+    #iterate through the weight vector and add data to results array
+    for j in range(len(weights)):
+        results[j+3,i] = weights[j]
+ 
+#convert results array to Pandas DataFrame
+results_frame = pd.DataFrame(results.T,columns=['ret','stdev','sharpe',stocks[0],stocks[1],stocks[2],stocks[3]])
+ 
+#locate position of portfolio with highest Sharpe Ratio
+max_sharpe_port = results_frame.iloc[results_frame['sharpe'].idxmax()]
+#locate positon of portfolio with minimum standard deviation
+min_vol_port = results_frame.iloc[results_frame['stdev'].idxmin()]
+ 
+#create scatter plot coloured by Sharpe Ratio
+plt.scatter(results_frame.stdev,results_frame.ret,c=results_frame.sharpe,cmap='RdYlBu')
+plt.xlabel('Volatility')
+plt.ylabel('Returns')
+plt.colorbar()
+#plot red star to highlight position of portfolio with highest Sharpe Ratio
+plt.scatter(max_sharpe_port[1],max_sharpe_port[0],marker=(5,1,0),color='r',s=1000)
+#plot green star to highlight position of minimum variance portfolio
+plt.scatter(min_vol_port[1],min_vol_port[0],marker=(5,1,0),color='g',s=1000)
+plt.show()
